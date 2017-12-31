456aeedb75252f8f0330f8b546aa68620e208774
Eduardo Pittol <edpittol@gmail.com>
2017-12-31 10:59:31 -0200

    Sticky header

--- app/assets/js/app/site-header.js
+++ app/assets/js/app/site-header.js
@@ -1,11 +1,40 @@
 /**
- * Load the background header
+ * Manipulate site header behavior
  */
 define([],function () {
-	var $siteHeader = jQuery('.site-header'),
-		bg = $siteHeader.data('bg');
+	/*
+	 * Load the background header
+	 */
+	var $siteHeader = jQuery( '.site-header' ),
+		bg = $siteHeader.data('bg'),
+		$topHeader = jQuery( '.top-header-wrapper' ),
+		stickyClass = 'top-header-wrapper__sticky';
 
 	if( '' !== bg ) {
-    	$siteHeader.css('background-image', 'url("' + bg + '")');
+    	$siteHeader.css( 'background-image', 'url("' + bg + '")' );
 	}
+
+	/*
+	 * Hide the navigation when open the search
+	 */
+	jQuery( '.search-toggle' ).on( 'click', function() {
+		jQuery( '.main-navigation' ).removeClass( 'toggled' );
+	} );
+
+	/*
+	 * Hide the search when open the navigation
+	 */
+	jQuery( '.menu-toggle' ).on( 'click', function() {
+		jQuery( '.site-search' ).removeClass( 'toggled' );
+	} );
+
+	jQuery( window )
+		.on( 'scroll', function() {
+			if( jQuery( this ).scrollTop() > 0 ) {
+				$topHeader.addClass( stickyClass );
+			} else {
+				$topHeader.removeClass( stickyClass );
+			}
+		} )
+		.trigger( 'scroll' );
 });
