bbf7d1c5f3be47c08fe91c59c12b12a850efbb27
Eduardo Pittol <edpittol@gmail.com>
2018-01-10 15:45:03 -0200

    Highlight 'Show All' category nav items when is home

--- app/template/template-parts/blog/category-nav.php
+++ app/template/template-parts/blog/category-nav.php
@@ -8,8 +8,15 @@ global $container;
 ?>
 <nav class="category-nav">
 	<ul>
-		<?php $home_url = $container->get( Aztec\Helper\Url::class )->get_post_page_url(); ?>
-		<li class="cat-item">
+		<?php 
+			$home_classes = array( 'cat-item' );
+			if( is_home() ) {
+				$home_classes[] = 'current-cat';
+			}
+			
+			$home_url = $container->get( Aztec\Helper\Url::class )->get_post_page_url();
+		?>
+		<li class="<?php echo esc_attr( join( ' ', $home_classes ) ) ?>">
 			<a href="<?php echo esc_url( $home_url ); ?>"><?php esc_html_e( 'Show All', 'elemarjr' ); ?></a>
 		</li>
 		<?php
