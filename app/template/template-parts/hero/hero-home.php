a3657b9913a7eec9936bff8cffb201d446e9ef79
Eduardo Pittol <edpittol@gmail.com>
2018-01-19 00:15:58 -0200

    Update home hero layout

--- app/template/template-parts/hero/hero-home.php
+++ app/template/template-parts/hero/hero-home.php
@@ -1,4 +1,4 @@
-<h1 class="hero--title"><?php echo esc_html( get_post_meta( get_the_ID(), 'hero_title', true ) ) ?></h1>
+<h1 class="hero--title"><?php echo wp_kses_post( get_post_meta( get_the_ID(), 'hero_title', true ) ) ?></h1>
 <p class="hero--description"><?php echo wp_kses_post( get_post_meta( get_the_ID(), 'hero_text', true ) ) ?></p>
 <a href="<?php echo esc_url( get_post_meta( get_the_ID(), 'hero_button_url', true ) ) ?>" class="hero--button button button__bordered">
 	<?php echo esc_html( get_post_meta( get_the_ID(), 'hero_button_label', true ) ) ?>
