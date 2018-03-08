8094c2605bdde42a5d334178172b233a2e511d77
Artur Comunello <arturcomunello@gmail.com>
2018-03-08 10:37:12 -0300

    Added navegation links previous and next inside post page

--- app/template/template-parts/blog/single/post-nav.php
+++ app/template/template-parts/blog/single/post-nav.php
@@ -2,12 +2,58 @@
 
 global $container;
 
+?>
+
+<div class="posts-nav-link">
+
+<?php
+
 if( $previous_post = get_adjacent_post( false, '', true ) ) {
-	$image_url = get_the_post_thumbnail_url( $post->ID, 'post-nav' );
-	$title = get_the_title( $post->ID );
+	$image_url = get_the_post_thumbnail_url( $previous_post->ID, 'post-nav' );
+	$title = get_the_title( $previous_post->ID );
+	$post_link = get_permalink( $previous_post->ID );
+
+?>
+	<a class="link-previous" href="<?php echo $post_link; ?>" title="">
+		<i class="i-arrow-right"></i>
+		Anterior
+	</a>
+
+	<a class="posts-nav-link--previous" href="<?php echo $post_link; ?>" title="">
+		<img class="posts-nav-img" src="<?php echo $image_url; ?>" alt="">
+		<div class="posts-nav-title-wrapper">
+			<p class="posts-nav-title"><?php echo $title ?></p>
+			<p class="posts-nav-link--cursor"> Anterior </p>
+		</div>
+	</a>
+
+<?php
+
 }
 
 if( $next_post = get_adjacent_post( false, '', false ) ) {
-	$image_url = get_the_post_thumbnail_url( $post->ID, 'post-nav' );
-	$title = get_the_title( $post->ID );
+	$image_url = get_the_post_thumbnail_url( $next_post->ID, 'post-nav' );
+	$title = get_the_title( $next_post->ID );
+	$post_link = get_permalink( $next_post->ID );
+?>
+	<a class="link-next" href="<?php echo $post_link; ?>" title="">
+		Próximo
+		<i class="i-arrow-right"></i>
+	</a>
+
+	<a class="posts-nav-link--next" href="<?php echo $post_link; ?>" title="">
+		<img class="posts-nav-img" src="<?php echo $image_url; ?>" alt="">
+		<div class="posts-nav-title-wrapper">
+			<p class="posts-nav-title"><?php echo $title ?></p>
+			<p class="posts-nav-link--cursor"> Próximo </p>
+		</div>
+
+	</a>
+
+<?php
+
 }
+
+?>
+
+</div>
