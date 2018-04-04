7968ee3b551b6119599a8183095274a6af1625aa
Eduardo Pittol <edpittol@gmail.com>
2018-04-04 13:25:33 -0300

    Remove unecessary body fields on About page

--- src/Integration/ACF/Pages/About.php
+++ src/Integration/ACF/Pages/About.php
@@ -39,7 +39,6 @@ class About extends Base {
 		if ( function_exists( 'acf_add_options_page' ) ) {
 			add_action( 'acf/include_fields', $this->callback( 'add_hero_fields' ) );
 			add_action( 'acf/include_fields', $this->callback( 'body_lines' ) );
-			add_action( 'acf/include_fields', $this->callback( 'add_body_fields' ) );
 		}
 	}
 
@@ -120,110 +119,6 @@ class About extends Base {
 					'key' => 'hero_text',
 					'name' => 'hero_text',
 					'label' => __( 'Text', 'elemarjr' ),
-				),
-			),
-			'location' => $this->location,
-		) );
-	}
-
-	/**
-	 * Add Purpose custom fields
-	 */
-	public function add_body_fields() {
-		acf_add_local_field_group( array(
-			'key' => 'about',
-			'title' => __( 'Sections', 'elemarjr' ),
-			'hide_on_screen' => array( 'the_content' ),
-			'fields' => array(
-				array(
-					'type' => 'text',
-					'key' => 'about_abilities_title',
-					'name' => 'about_abilities_title',
-					'label' => __( 'My Abilities', 'elemarjr' ) . ' - ' . __( 'Title', 'elemarjr' ),
-				),
-				array(
-					'type' => 'wysiwyg',
-					'key' => 'about_abilities_text',
-					'name' => 'about_abilities_text',
-					'label' => __( 'My Abilities', 'elemarjr' ) . ' - ' . __( 'Text', 'elemarjr' ),
-				),
-				array(
-					'type' => 'image',
-					'key' => 'about_abilities_image',
-					'name' => 'about_abilities_image',
-					'label' => __( 'My Abilities', 'elemarjr' ) . ' - ' . __( 'Image', 'elemarjr' ),
-				),
-				array(
-					'type' => 'text',
-					'key' => 'about_clients_title',
-					'name' => 'about_clients_title',
-					'label' => __( 'My Clients', 'elemarjr' ) . ' - ' . __( 'Title', 'elemarjr' ),
-				),
-				array(
-					'type' => 'wysiwyg',
-					'key' => 'about_clients_text',
-					'name' => 'about_clients_text',
-					'label' => __( 'My Clients', 'elemarjr' ) . ' - ' . __( 'Text', 'elemarjr' ),
-				),
-				array(
-					'type' => 'image',
-					'key' => 'about_clients_image',
-					'name' => 'about_clients_image',
-					'label' => __( 'My Clients', 'elemarjr' ) . ' - ' . __( 'Image', 'elemarjr' ),
-				),
-				array(
-					'type' => 'text',
-					'key' => 'about_expirience_title',
-					'name' => 'about_expirience_title',
-					'label' => __( 'My Experience', 'elemarjr' ) . ' - ' . __( 'Title', 'elemarjr' ),
-				),
-				array(
-					'type' => 'wysiwyg',
-					'key' => 'about_expirience_text',
-					'name' => 'about_expirience_text',
-					'label' => __( 'My Experience', 'elemarjr' ) . ' - ' . __( 'Text', 'elemarjr' ),
-				),
-				array(
-					'type' => 'image',
-					'key' => 'about_expirience_image',
-					'name' => 'about_expirience_image',
-					'label' => __( 'My Experience', 'elemarjr' ) . ' - ' . __( 'Image', 'elemarjr' ),
-				),
-				array(
-					'type' => 'text',
-					'key' => 'about_raven_title',
-					'name' => 'about_raven_title',
-					'label' => __( 'RavenDB', 'elemarjr' ) . ' - ' . __( 'Title', 'elemarjr' ),
-				),
-				array(
-					'type' => 'wysiwyg',
-					'key' => 'about_raven_text',
-					'name' => 'about_raven_text',
-					'label' => __( 'RavenDB', 'elemarjr' ) . ' - ' . __( 'Text', 'elemarjr' ),
-				),
-				array(
-					'type' => 'image',
-					'key' => 'about_raven_image',
-					'name' => 'about_raven_image',
-					'label' => __( 'RavenDB', 'elemarjr' ) . ' - ' . __( 'Image', 'elemarjr' ),
-				),
-				array(
-					'type' => 'text',
-					'key' => 'about_mvp_title',
-					'name' => 'about_mvp_title',
-					'label' => __( 'Most Valuable Professional', 'elemarjr' ) . ' - ' . __( 'Title', 'elemarjr' ),
-				),
-				array(
-					'type' => 'wysiwyg',
-					'key' => 'about_mvp_text',
-					'name' => 'about_mvp_text',
-					'label' => __( 'Most Valuable Professional', 'elemarjr' ) . ' - ' . __( 'Text', 'elemarjr' ),
-				),
-				array(
-					'type' => 'image',
-					'key' => 'about_mvp_image',
-					'name' => 'about_mvp_image',
-					'label' => __( 'Most Valuable Professional', 'elemarjr' ) . ' - ' . __( 'Image', 'elemarjr' ),
 				),
 			),
 			'location' => $this->location,
