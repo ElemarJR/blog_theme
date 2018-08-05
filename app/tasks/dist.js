8e596b79d839dab1b0a012877ffcf37d2be15f0d
Eduardo Pittol <edpittol@gmail.com>
2018-08-05 00:00:36 -0300

    Fix grunt dist task

--- app/tasks/dist.js
+++ app/tasks/dist.js
@@ -7,12 +7,7 @@ module.exports = function ( grunt ) {
 	grunt.task.registerTask( 'dist', [
 		'stylus:dist',
 		'requirejs:compile',
-		'sync:adminjs',
-		'sync:font-icon',
 		'imagemin',
-		'symlink:theme',
-		'symlink:assets',
-		'symlink:cli',
-		'symlink:wp'
+		'symlink'
 	] );
 };
